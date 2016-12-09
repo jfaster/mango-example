1ec05f28351c9bb93226923f795ae5adb5678775
liangyanghe <liangyanghe@le.com>
2016-12-09 14:55:06 +0800

    stat示例

---
+++ src/main/java/org/jfaster/mango/example/stat/StatAlarm.java
@@ -0,0 +1,47 @@
+package org.jfaster.mango.example.stat;
+
+import org.jfaster.mango.datasource.DriverManagerDataSource;
+import org.jfaster.mango.operator.Mango;
+import org.jfaster.mango.stat.OperatorStat;
+import org.jfaster.mango.stat.StatMonitor;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author ash
+ */
+public class StatAlarm {
+
+    public static void main(String[] args) {
+        String driverClassName = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/mango_example";
+        String username = "root"; // 这里请使用您自己的用户名
+        String password = "root"; // 这里请使用您自己的密码
+        DataSource ds = new DriverManagerDataSource(driverClassName, url, username, password);
+        Mango mango = Mango.newInstance(ds); // 使用数据源初始化mango
+
+        mango.setStatMonitor(new StatMonitor() {
+
+            @Override
+            public void handleStat(long statBeginTime, long statEndTime, List<OperatorStat> stats) throws Exception {
+                for (OperatorStat stat : stats) {
+                    if (stat.getAverageDatabaseExecutePenalty() > TimeUnit.MILLISECONDS.toNanos(10)) {
+                        // 有DAO接口在10秒内平均响应时间大于10毫秒，促发短信或邮件报警
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public int periodSecond() {
+                return 10;
+            }
+
+        });
+
+        // 后续创建DAO，执行DAO的代码
+    }
+
+}
---
+++ src/main/java/org/jfaster/mango/example/stat/StatOut.java
@@ -0,0 +1,67 @@
+package org.jfaster.mango.example.stat;
+
+import org.jfaster.mango.datasource.DriverManagerDataSource;
+import org.jfaster.mango.operator.Mango;
+import org.jfaster.mango.stat.OperatorStat;
+import org.jfaster.mango.stat.StatMonitor;
+
+import javax.sql.DataSource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author ash
+ */
+public class StatOut {
+
+    public static void main(String[] args) {
+        String driverClassName = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/mango_example";
+        String username = "root"; // 这里请使用您自己的用户名
+        String password = "root"; // 这里请使用您自己的密码
+        DataSource ds = new DriverManagerDataSource(driverClassName, url, username, password);
+        Mango mango = Mango.newInstance(ds); // 使用数据源初始化mango
+
+        mango.setStatMonitor(new StatMonitor() {
+
+            @Override
+            public void handleStat(long statBeginTime, long statEndTime, List<OperatorStat> stats) throws Exception {
+                StringBuilder data = new StringBuilder();
+                data.append("Performance Statistics  [")
+                        .append(format(statBeginTime))
+                        .append("] - [")
+                        .append(format(statEndTime))
+                        .append("]")
+                        .append("\n");
+                data.append(String.format("%-36s%-12s%-12s%-12s%n",
+                        "dao", "avg", "total", "error"));
+                for (OperatorStat stat : stats) {
+                    if (stat.getDatabaseExecuteCount() > 0) { // 执行db数大于0才统计
+                        String dao = stat.getMethod().getDeclaringClass().getSimpleName() + "." + stat.getMethod().getName();
+                        data.append(String.format("%-36s%-12.1f%-12s%-12s%n",
+                                dao,
+                                (double) stat.getAverageDatabaseExecutePenalty() / (1000*1000),
+                                stat.getDatabaseExecuteCount(),
+                                stat.getDatabaseExecuteExceptionCount()));
+                    }
+                }
+                System.out.println(data);
+            }
+
+            @Override
+            public int periodSecond() {
+                return 10;
+            }
+
+            private String format(long time) {
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                return format.format(new Date(time));
+            }
+
+        });
+
+        // 后续创建DAO，执行DAO的代码
+    }
+
+}

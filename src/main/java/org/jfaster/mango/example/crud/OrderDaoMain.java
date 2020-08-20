e702fa69c475f8a2352b667ba2633b7589ec81dd
liangyanghe <ash266@163.com>
2020-08-20 10:40:17 +0800

    最新的CRUD操作

--- src/main/java/org/jfaster/mango/example/crud/OrderDaoMain.java
+++ src/main/java/org/jfaster/mango/example/crud/OrderDaoMain.java
@@ -1,5 +1,7 @@
 package org.jfaster.mango.example.crud;
 
+import org.jfaster.mango.annotation.DB;
+import org.jfaster.mango.crud.CrudDao;
 import org.jfaster.mango.datasource.DriverManagerDataSource;
 import org.jfaster.mango.operator.Mango;
 
@@ -18,17 +20,21 @@ public class OrderDaoMain {
     DataSource ds = new DriverManagerDataSource(driverClassName, url, username, password);
     Mango mango = Mango.newInstance(ds); // 使用数据源初始化mango
 
-    OrderNoSqlDao dao = mango.create(OrderNoSqlDao.class);
+    OrderDao dao = mango.create(OrderDao.class);
     Order order = new Order();
     order.setUid(100);
     order.setStatus(1);
     dao.add(order);
-    int id = dao.addAndReturnGeneratedId(order);
+    int id = (int) dao.addAndReturnGeneratedId(order);
     order.setId(id);
     System.out.println(dao.getOne(id));
     order.setStatus(2);
     dao.update(order);
     System.out.println(dao.getOne(id));
+  }
+
+  @DB(table = "t_order")
+  interface OrderDao extends CrudDao<Order, Integer> {
   }
 
 }
